028cf2f725987ff6837879f313580995e4e15c8c
YagamiRin <solikejesus@qq.com>
2022-03-30 18:22:47 +0800

    111

---
+++ YYDJ-TJ/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+
+namespace YYDJ_TJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FaceRec FaceRec = new FaceRec();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FaceRec.openCamera(pictureBox1,pictureBox2);
+        }
+    }
+}
